20b6e4355994b2b6921d35cf0596572ad1e878cd
Tomas Di Leo <tomasjdle@gmail.com>
2024-10-23 23:49:54 -0300

    Ajusto los margenes de deteccion de obstaculos y cambio el pin 6, queda prohibido usar el pin 6 ya que no se puede usar para lo que queremos sin que active un reset

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,12 @@
 #define IZQUIERDA_PWM 25
 
 // sensor
-#define TRIG_PIN_1 34  
-#define ECHO_PIN_1 5
+#define TRIG_PIN_1 5
+#define ECHO_PIN_1 34
 #define TRIG_PIN_2 33 
-#define ECHO_PIN_2 6    
-#define MAX_DISTANCIA_SENSOR 50 //(50 cm)
-#define DISTANCIA_OBSTACULO 20  //(20cm)
+#define ECHO_PIN_2 10    
+#define MAX_DISTANCIA_SENSOR 80 //(80 cm)
+#define DISTANCIA_OBSTACULO 30  //(30cm)
 
 NewPing sensor1(TRIG_PIN_1, ECHO_PIN_1, MAX_DISTANCIA_SENSOR);
 NewPing sensor2(TRIG_PIN_2, ECHO_PIN_2, MAX_DISTANCIA_SENSOR);
@@ -66,7 +66,7 @@ void loopSensores(void *parameter){
 
 void setup(){
 
-    xTaskCreatePinnedToCore(loopSensores, "TaskSensores", 8000, NULL, 1, &TaskSensores, 0);
+    xTaskCreatePinnedToCore(loopSensores, "TaskSensores", 10000, NULL, 1, &TaskSensores, 0);
     
     Serial.begin(115200);
 
