2246b83d5c3e130f0f8f0e5e4163d50a78a5c24f
Tomas Di Leo <tomasjdle@gmail.com>
2024-10-31 22:59:52 -0300

    Refactor de nombre de un test y el archivo correspondiente

--- test/test_Ruedas.cpp
+++ test/test_Ruedas.cpp
@@ -11,18 +11,14 @@ void setUp(void)
 }
 void tearDown(void) {}
 
-void test_loop(void)
+void test_inicializar(void)
 {
-    // Create an instance of Ruedas with arbitrary pin numbers
     Ruedas ruedas(3, 4, 5);
 
-    // Stub pinMode function
     When(Method(ArduinoFake(), pinMode).Using(_, _)).AlwaysReturn();
 
-    // Call the method
     ruedas.inicializar();
 
-    // Verify pinMode was called with each pin and set to OUTPUT
     Verify(Method(ArduinoFake(), pinMode).Using(3, OUTPUT)).Once();
     Verify(Method(ArduinoFake(), pinMode).Using(4, OUTPUT)).Once();
     Verify(Method(ArduinoFake(), pinMode).Using(5, OUTPUT)).Once();
@@ -31,47 +27,36 @@ void test_loop(void)
 void test_moverAdelante() {
     Ruedas ruedas(3, 4, 5);
 
-    // Stub digitalWrite and analogWrite
     When(Method(ArduinoFake(), digitalWrite).Using(_, _)).AlwaysReturn();
     When(Method(ArduinoFake(), analogWrite).Using(_, _)).AlwaysReturn();
 
-    // Call the method with a speed of 100
     ruedas.moverAdelante(100);
 
-    // Verify the function calls
     Verify(Method(ArduinoFake(), digitalWrite).Using(3, LOW)).Once();
     Verify(Method(ArduinoFake(), digitalWrite).Using(4, HIGH)).Once();
     Verify(Method(ArduinoFake(), analogWrite).Using(5, 100)).Once();
 }
 
-// Test case for the `moverAtras` method
 void test_moverAtras() {
     Ruedas ruedas(3, 4, 5);
 
-    // Stub functions
     When(Method(ArduinoFake(), digitalWrite).Using(_, _)).AlwaysReturn();
     When(Method(ArduinoFake(), analogWrite).Using(_, _)).AlwaysReturn();
 
-    // Call the method with a speed of 150
     ruedas.moverAtras(150);
 
-    // Verify the function calls
     Verify(Method(ArduinoFake(), digitalWrite).Using(3, HIGH)).Once();
     Verify(Method(ArduinoFake(), digitalWrite).Using(4, LOW)).Once();
     Verify(Method(ArduinoFake(), analogWrite).Using(5, 150)).Once();
 }
 
-// Test case for the `detener` method
 void test_detener() {
     Ruedas ruedas(3, 4, 5);
 
-    // Stub digitalWrite
     When(Method(ArduinoFake(), digitalWrite).Using(_, _)).AlwaysReturn();
 
-    // Call the method
     ruedas.detener();
 
-    // Verify the function calls
     Verify(Method(ArduinoFake(), digitalWrite).Using(3, LOW)).Once();
     Verify(Method(ArduinoFake(), digitalWrite).Using(4, LOW)).Once();
 }
@@ -80,7 +65,7 @@ int main(int argc, char **argv)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_loop);
+    RUN_TEST(test_inicializar);
     RUN_TEST(test_moverAdelante);
     RUN_TEST(test_moverAtras);
     RUN_TEST(test_detener);
