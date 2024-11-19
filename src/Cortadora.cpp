e291b9bd179f0f42545dbb5ef20976014facee26
Tomas Di Leo <tomasjdle@gmail.com>
2024-11-19 14:06:02 -0300

    La cortadora ahora se inicia por medio de PWM en aproximadamente 250ms para evitar un gran pico de corriente

--- src/Cortadora.cpp
+++ src/Cortadora.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
 #include "Cortadora.h"
 
-Cortadora::Cortadora(int pinA, int pinB){
+const int tiempoDeInicio = 1000;
+const int valorMaximo = 255;
+const int paso = 5;
+
+Cortadora::Cortadora(int pinA, int pinB, int pinPWM){
     this->pinA = pinA;
     this->pinB = pinB;
+    this->pinPWM = pinPWM;
+}
+
+void Cortadora::inicializar(){
+    pinMode(pinA, OUTPUT);
+    pinMode(pinB, OUTPUT);
 }
 
 void Cortadora::cortar(){
-    digitalWrite(pinA, HIGH);
+    while(valorPWM < valorMaximo){
+        valorPWM += paso;
+        digitalWrite(pinA, HIGH);
+        digitalWrite(pinB, LOW);
+        analogWrite(pinPWM, valorPWM);
+        delay(5);
+    }
+}
+
+void Cortadora::parar(){
+    valorPWM = 0;
+    digitalWrite(pinA, LOW);
     digitalWrite(pinB, LOW);
 }
--- src/Cortadora.h
+++ src/Cortadora.h
@@ -5,10 +5,14 @@ class Cortadora {
 private:
     int pinA;
     int pinB;
+    int pinPWM;
+    int valorPWM;
 
 public:
-    Cortadora(int pinA, int pinB);
+    Cortadora(int pinA, int pinB, int pinPWM);
+    void inicializar();
     void cortar();
+    void parar();
 }; 
 
 #endif
